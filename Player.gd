37921cd37a02b5f7af5dfcda534163ff6295baeb
Justin Lavin <jlavin@olivetcollege.edu>
2019-01-21 15:11:11 -0500

    added player texture and fixed movement

--- Player.gd
+++ Player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 10
-const ACCELERATION = 2
+const MAX_SPEED = 2
+const ACCELERATION = .5
+const DECELERATION = 7
 var direction = Vector2()
 var motion = Vector2()
 
@@ -10,28 +11,32 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		motion.y = min(motion.y - ACCELERATION, -MAX_SPEED)
 		direction = Vector2(0,-1)
+		$Sprite.play("Up")
 	elif Input.is_action_pressed("ui_down"):
 		motion.y = max(motion.y + ACCELERATION, MAX_SPEED)
 		direction = Vector2(0,1)
+		$Sprite.play("Down")
 	else:
-		if motion.y > 10:
-			motion.y -= 10
-		elif motion.y < -10:
-			motion.y += 10
+		if motion.y > MAX_SPEED:
+			motion.y -= DECELERATION
+		elif motion.y < -MAX_SPEED:
+			motion.y += DECELERATION
 		else:
 			motion.y = 0
 	
 	if Input.is_action_pressed("ui_left"):
 		motion.x = min(motion.x - ACCELERATION, -MAX_SPEED)
 		direction = Vector2(-1,0)
+		$Sprite.play("Left")
 	elif Input.is_action_pressed("ui_right"):
 		motion.x = max(motion.x + ACCELERATION, MAX_SPEED)
 		direction = Vector2(1,0)
+		$Sprite.play("Right")
 	else:
-		if motion.x > 10:
-			motion.x -= 10
-		elif motion.x < -10:
-			motion.x += 10
+		if motion.x > 0:
+			motion.x -= DECELERATION
+		elif motion.x < 0:
+			motion.x += DECELERATION
 		else:
 			motion.x = 0
 	
